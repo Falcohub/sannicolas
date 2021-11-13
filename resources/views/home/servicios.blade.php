5bdbd82838b815f339741bbad8cd0a299cf69e5d
Falco <luis.falcohe@amigo.edu.co>
2021-11-12 23:55:29 -0500

    Avance pagina servicios

--- resources/views/home/servicios.blade.php
+++ resources/views/home/servicios.blade.php
@@ -1,12 +1,123 @@
 <x-app-layout>
 
     <div class="h-96 bg-black">
-        <div class="h-full w-full bg-no-repeat opacity-30 bg-fixed bg-cover" style="background-image: url(https://images.unsplash.com/photo-1595076054731-dae41bd28715?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80)">
-        
-            <div>
-                <h1 class="text-center text-2xl sm:text-4xl md:text-5xl font-bold mb-10 font-sans">Nuestra empresa</h1>
+        <div class="h-full w-full bg-no-repeat opacity-30 bg-fixed bg-cover flex items-center justify-center"
+            style="background-image: url(https://cdn.pixabay.com/photo/2016/09/27/02/11/cemetery-1697469_960_720.jpg)">
+            <div class="container">
+                <h1 class="text-center text-2xl sm:text-4xl md:text-5xl font-bold mb-10 font-sans text-white">Nuestros
+                    Servicios</h1>
             </div>
-        </div>  
+        </div>
     </div>
+
+    <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero"
+                    src="https://www.sannicolascasadefunerales.com/wp-content/uploads/2021/09/Nuestros-servicios-Casa-de-Funerales-San-Nicolas.png"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Servicios Proexequiales</h1>
+                <p class="mb-8 leading-relaxed">Creemos que anticiparse a planear y tomar decisiones sobre situaciones
+                    que a futuro debemos resolver, es un acto de responsabilidad y de amor. Hacia las personas que más
+                    queremos, por eso en San Nicolás creamos diferentes planes que se acomodan a tu necesidad. Si eres
+                    de las personas que de forma anticipada busca prever situaciones inesperadas, pregúntanos por la
+                    Previsión Exequial, ofrecemos diferentes planes.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="http://artesaniasecuatorianas.com/wp-content/uploads/2021/05/IMG_20210504_053522.jpg"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Cremación</h1>
+                <p class="mb-8 leading-relaxed">Los usuarios que consideren acceder a este servicio, en San Nicolás
+                    realizamos el traslado directamente a la agencia. Finalizado el procedimiento, entregamos las
+                    cenizas en su respectiva urna, este procedimiento incluye un ritual simbólico para los familiares y
+                    amigos más cercanos.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Salas de Velación</h1>
+                <p class="mb-8 leading-relaxed">En nuestras instalaciones usted podrá realizar ceremonias de despedida
+                    cómodamente. Recibiendo asistencia de cafetería, elementos de velación o musicalización
+                    personalizada en caso de que lo requiera, también podrá acceder a nuestro servicios de rituales, en
+                    donde planeamos un momento simbólico que le permita a familiares y amigos una sana despedida.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Velación en Casa</h1>
+                <p class="mb-8 leading-relaxed">Sabemos que algunas familias prefieren la comodidad de su hogar. Por
+                    este servicio les ofrece el traslado de carpas, silletería, ornamentos para las flores y el altar
+                    hasta el lugar de velación, incluyendo el servicio de cafetería, elementos de velación y oración y/o
+                    homenaje simbólico de despedida. Lo que más no interesa es poder brindar soluciones a nuestros
+                    clientes.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="https://cdn.pixabay.com/photo/2018/04/03/19/30/flower-3287768_960_720.jpg"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Floristería</h1>
+                <p class="mb-8 leading-relaxed">San Nicolás reconoce que son las flores el acto de ofrenda. Para
+                    entregar a los seres que amas y a través de ellas se materializan mensajes de aprecio, presencia y
+                    solidaridad con el otro, permítanos ser los encargados de materializar los sentimiento que lo
+                    motivan a regalar un ramo de flores, contáctese con nosotros.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="https://www.sannicolascasadefunerales.com/wp-content/uploads/2018/08/IM-9-Nuestros-servicios-Casa-de-Funerales-San-Nicolas.jpg"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Atención Psicológica</h1>
+                <p class="mb-8 leading-relaxed">Sabemos que en el momento en que se despide a un ser querido, familiares
+                    y amigos quedan afectados por su partida. Son ellos quienes requieren de toda nuestra atención, por
+                    eso en San Nicolás ponemos a tu disposición un profesional que acompañará a las personas afectadas
+                    durante la fase de duelo, facilitándoles un despertar de consciencia que les permita gestionar de
+                    forma correcta sus emociones, y así evitar enfermedades o hechos traumáticos en la familia.</p>
+            </div>
+        </div>
+
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"
+                    style="height: 500px; width: 500px;">
+            </div>
+            <div
+                class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Transporte</h1>
+                <p class="mb-8 leading-relaxed">Un hecho inesperado puede ocurrir lejos de casa. Por eso nuestra familia
+                    empresarial tiene la experiencia y el conocimiento suficiente para prestar servicios de transporte
+                    terrestre, aéreo y marítimo, atendiendo con profesionalismo en todo el territorio colombiano.</p>
+            </div>
+        </div>
+    </section>
 
 </x-app-layout>
